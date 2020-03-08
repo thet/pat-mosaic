d9fe2a293a7c1d3024acb62169adcd2aaeb3792e
Johannes Raggam <thetetet@gmail.com>
2020-03-08 13:39:19 +0100

    begin multi-column support

--- src/pat-mosaic.js
+++ src/pat-mosaic.js
@@ -4,11 +4,23 @@ define(['pat-base'], function(Base) {
     name: 'mosaic',
     trigger: '.pat-mosaic',
     current_resize: null,
+    max_cols: 4,
 
     init: function() {
+
+      // Insert column break elements.
+      var el = this.$el[0];
+      var el_break = null;
+      for (var i = 0; i < this.max_cols - 1; i++) {
+        el_break = document.createElement('span');
+        el_break.setAttribute('class', 'pat-mosaic__break');
+        el.appendChild(el_break);
+      }
+
       this.calculate_heights();
+
+      // calculate heights after window resize
       window.addEventListener('resize', function (e) {
-        // calculate heights after window resize
         if (this.current_resize !== null) {
           window.clearTimeout(this.current_resize);
         }
