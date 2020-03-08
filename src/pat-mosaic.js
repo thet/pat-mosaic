5b068fef10982e70fabef2479dc56a8c8303a424
Johannes Raggam <thetetet@gmail.com>
2020-03-08 20:38:11 +0100

    Set vertical margins in a row to the same as the gap between columns.

--- src/pat-mosaic.js
+++ src/pat-mosaic.js
@@ -31,12 +31,35 @@ define(['pat-base'], function(Base) {
     },
 
     calculate_heights: function () {
-      var heights = {};
-      var max_height = 0;
+
+      // Get all items in a object with column number as key
+      var order_items = {};
       this.$el[0].querySelectorAll('.pat-mosaic__item').forEach(function (it) {
         var style = getComputedStyle(it);
-        var height = it.getBoundingClientRect().height + parseInt(style.marginTop, 10) + parseInt(style.marginBottom, 10);
-        heights[style.order] = (heights[style.order] || 0) + height;
+        if (! order_items[style.order]) {
+          order_items[style.order] = [];
+        }
+        order_items[style.order].push(it);
+      });
+
+      // Calculate gap between two columns.
+      // Used to calculate the margin between elements in one column to be the same as the gap between columns.
+      var gap = null;
+      if ("2" in order_items) {
+        var rect1 = order_items["1"][0].getBoundingClientRect();
+        var rect2 = order_items["2"][0].getBoundingClientRect();
+        gap = rect2.left - rect1.right;
+      }
+
+      var heights = {};
+      var max_height = 0;
+      Object.keys(order_items).forEach(function (key) {
+        order_items[key].forEach(function (it) {
+          it.style.marginBottom = gap + 'px'; // Same margin as gab btw columns.
+          var style = getComputedStyle(it);
+          var height = it.getBoundingClientRect().height + parseInt(style.marginTop, 10) + gap;
+          heights[style.order] = (heights[style.order] || 0) + height;
+        });
       });
       Object.values(heights).forEach(function (val) {
         if (val>max_height) {
