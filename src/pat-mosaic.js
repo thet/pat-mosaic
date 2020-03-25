94b4d1d4732b7f32d2378d462f8fd54eb9f480c0
Johannes Raggam <thetetet@gmail.com>
2020-03-25 01:19:11 +0100

    For correct gap calculations we need the items to be already laid out correctly. Or do the calculation based on width; however. Then calulcate gaps and do the height calculation again, including gaps.

--- src/pat-mosaic.js
+++ src/pat-mosaic.js
@@ -4,6 +4,7 @@ define(['pat-base'], function(Base) {
     name: 'mosaic',
     trigger: '.pat-mosaic',
     current_resize: null,
+    even_margins: true,
 
     init: function() {
 
@@ -32,29 +33,45 @@ define(['pat-base'], function(Base) {
         order_items[key].push(it);
       });
 
-      // Calculate gap between two columns.
-      // Used to calculate the margin between elements in one column to be the same as the gap between columns.
-      var gap = null;
-      if ("2" in order_items) {
-        var rect1 = order_items["1"][0].getBoundingClientRect();
-        var rect2 = order_items["2"][0].getBoundingClientRect();
-        gap = rect2.left - rect1.right;
-      } else {
+      if (Object.keys(order_items).length === 1) {
         // 1-column layout. nothing to do.
-        this.$el[0].style.height = null;
-        // Done.
         this.$el[0].classList.add('pat-mosaic--layout-done');
-        return;
       }
 
-      // Set correct container height
+      var max_height = this._calculate_heights(order_items);
+      this.$el[0].style.height = max_height + "px";
+
+      // Done. Almost. Need container to be set to 100% now.
+      this.$el[0].classList.add('pat-mosaic--layout-done');
+
+      if (this.even_margins) {
+        // Calculate gap between two columns.
+        // Used to calculate the margin between elements in one column to be the same as the gap between columns.
+        var gap = null;
+        if ("2" in order_items) {
+          var rect1 = order_items["1"][0].getBoundingClientRect();
+          var rect2 = order_items["2"][0].getBoundingClientRect();
+          gap = rect2.left - rect1.right;
+        }
+        var max_height = this._calculate_heights(order_items, gap);
+        this.$el[0].style.height = max_height + "px";
+      }
+
+    },
+
+    _calculate_heights: function (order_items, gap) {
+      // Set correct container height / round 1, without calculated margins
       var heights = {};
       var max_height = 0;
       Object.keys(order_items).forEach(function (key) {
         order_items[key].forEach(function (it) {
-          it.style.marginBottom = gap + 'px'; // Same margin as gab btw columns.
           var style = getComputedStyle(it);
-          var height = it.getBoundingClientRect().height + parseInt(style.marginTop, 10) + gap;
+          if (gap) {
+            it.style.marginBottom = gap + 'px'; // Same margin as gap btw columns.
+          } else {
+            gap = parseInt(style.marginBottom, 10);
+          }
+          var height = it.getBoundingClientRect().height + parseInt(style.marginTop, 10)  + gap;
           heights[key] = (heights[key] || 0) + height;
         });
       });
@@ -63,10 +80,7 @@ define(['pat-base'], function(Base) {
           max_height = val;
         }
       });
-      this.$el[0].style.height = max_height + "px";
-
-      // Done.
-      this.$el[0].classList.add('pat-mosaic--layout-done');
+      return max_height;
     },
 
   });
